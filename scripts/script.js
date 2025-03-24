0a82ff2377287523e0b7e4cd18c4a851520c1b0b
Christian Pohlschneider <c.pohlschneider@t-online.de>
2025-03-24 22:07:31 +0100

    show info on pokeCard

--- scripts/script.js
+++ scripts/script.js
@@ -14,9 +14,19 @@ function getPromise() {
     });
 }
 
-function renderPokeCards(initArray, index, data){
+function renderPokeCards(initArray, index, data) {
     document.getElementById("content").innerHTML += renderPokeCard(initArray, index);
-    document.getElementById("contentImg" + index).src = data.sprites.other["official-artwork"].front_default;
+    document.getElementById("contentImg" + index).src = data.sprites.other.home.front_default;
+    document.getElementById("contentImg" + index).classList.add(data.types[0].type.name);
+    renderPokeType(initArray, index, data);
+}
+
+function renderPokeType(initArray, index, data) {
+for (let typeIndex = 0; typeIndex < data.types.length; typeIndex++) {
+    document.getElementById("pokemonType" + index).innerHTML += renderSpecificPokeType(initArray, index, typeIndex);
+    document.getElementById("typeImg" + index + "#" + typeIndex).src = "./assets/icons/" + data.types[typeIndex].type.name + ".png";
+    
+}
 }
 
 function toggleMenu(id) {
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,7 +1,18 @@
 function renderPokeCard(initArray, index) {
     return `
-    <div class="pokeCard">${JSON.stringify(initArray[index].name)}
-    <img id="contentImg${index}" src="" alt="pokemon">
+    <div class="pokeCard">
+        <div class="pokeCardHeader" id="pokeCardHeader">
+            <p class="pokeID">#${Math.abs(Number(initArray[index].id) + 1)}</p>
+            <p>${initArray[index].name}</p>
+        </div>
+        <img class="contentImg" id="contentImg${index}" src="" alt="pokemon">
+        <div class="pokemonType" id="pokemonType${index}"></div>
     </div>
+    `;
+}
+
+function renderSpecificPokeType(initArray, index, typeIndex){
+    return `
+    <img class="pokeTypeImg" id="typeImg${index + "#" + typeIndex}" src="" alt="type">
     `;
 }
